86f0a546285b09b690e5119d218c4911aed3ad04
Omega B. Dziko Fuentes <omegab.dzikofuentes@MacBookAir.lan>
2025-03-17 20:33:23 -0400

    tracks up

--- app/models/track.py
+++ app/models/track.py
@@ -8,7 +8,7 @@ class Track(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
+    title = db.Column(db.String(255), nullable=True)
     audio_url = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     duration = db.Column(db.Integer)
